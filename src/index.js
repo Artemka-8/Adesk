771dd70e33bb3c832894db5d7a004c08a1c5edec
Artemka-8 <136094244+Artemka-8@users.noreply.github.com>
2023-11-01 10:23:56 +0500

    project-completion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style/style.css'
+
+/* Подвал мобильной версии сайта */
+
+document.addEventListener('DOMContentLoaded', () => {
+	const accordions = document.querySelectorAll('.accordion');
+
+	accordions.forEach(el => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const control = self.querySelector('.accordion__control');
+			const content = self.querySelector('.accordion__content');
+
+			self.classList.toggle('open');
+		});
+	});
+});
